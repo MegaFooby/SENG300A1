58a21b74f7b0760fe5415930d6141205d3eef981
MegaFooby <32686255+MegaFooby@users.noreply.github.com>
2017-10-21 13:16:22 -0600

    Add files via upload

--- CoinReceptacleListening.java
+++ CoinReceptacleListening.java
@@ -5,12 +5,10 @@ import org.lsmr.vending.hardware.*;
 public class CoinReceptacleListening implements CoinReceptacleListener {
 	private boolean isOn;
 	private int value;
-	private Coin[] coins;
 	private int coinCount;
 	
 	public CoinReceptacleListening(int maxCap) {
 		isOn = true;
-		coins = null;
 		value = 0;
 		coinCount = 0;
 	}
@@ -24,54 +22,29 @@ public class CoinReceptacleListening implements CoinReceptacleListener {
 	}
 
 	public void coinAdded(CoinReceptacle receptacle, Coin coin) {
-		if(coins == null) {
-			coins = new Coin[receptacle.getCapacity()];
-		}
-		coins[coinCount] = coin;
 		coinCount++;
 		value += coin.getValue();
 	}
 
 	public void coinsRemoved(CoinReceptacle receptacle) {
-		if(coins == null) {
-			coins = new Coin[receptacle.getCapacity()];
-		}
 		value = 0;
-		for(int i = 0; i < coinCount; i++) {
-			coins[i] = null;
-		}
 		coinCount = 0;
 	}
 
 	public void coinsFull(CoinReceptacle receptacle) {}
 
 	public void coinsLoaded(CoinReceptacle receptacle, Coin... coins) {
-		if(this.coins == null) {
-			this.coins = new Coin[receptacle.getCapacity()];
+		for(Coin coin : coins) {
+			value += coin.getValue();
+			coinCount++;
 		}
 	}
 
 	public void coinsUnloaded(CoinReceptacle receptacle, Coin... coins) {
-		if(this.coins == null) {
-			this.coins = new Coin[receptacle.getCapacity()];
+		for(Coin coin:coins) {
+			value -= coin.getValue();
+			coinCount--;
 		}
-		for(Coin rmCoin:coins) {
-			for(Coin rmHere:this.coins) {
-				if(rmHere != null && rmHere.getValue() == rmCoin.getValue()) {
-					value -= rmHere.getValue();
-					coinCount--;
-					rmHere = null;
-					break;
-				}
-			}
-		}
-		for(int i = 0; i < receptacle.getCapacity()-1; i++) {
-			if(coins[i] == null) {
-				coins[i] = coins[i+1];
-				coins[i+1] = null;
-			}
-		}
-		coins[receptacle.getCapacity()-1] = null;
 	}
 	
 	public void Purchase(int amount) {
@@ -84,6 +57,10 @@ public class CoinReceptacleListening implements CoinReceptacleListener {
 	
 	public boolean isOn() {
 		return isOn;
+	}
+	
+	public int coinCount() {
+		return coinCount;
 	}
 
 }
